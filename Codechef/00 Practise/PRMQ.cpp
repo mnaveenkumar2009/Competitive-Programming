ae3f6070938f7eb81900e9444e2fb3f682a8ba83
naveen <mnaveenkumar2009@gmail.com>
2018-04-15 17:12:19 +0530

    added template

---
+++ Codechef/00 Practise/PRMQ.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f(i,n) for(i=0;i<n;i++)
+#define pb push_back
+#define sd(n) scanf("%d",&n)
+#define sc(n) scanf("%c",&n)
+#define slld(n) scanf("%lld",&n)
+#define mod 1000000007
+#define mp make_pair
+#define ff first
+#define ss second
+#define ll long long
+#define ld long double
+#define gc getchar
+#define pc putchar
+inline ll uscan()
+{
+    ll n=0,c=gc();
+while(c<'0'||c>'9')
+c=gc();
+while(c<='9'&&c>='0'){
+n=n*10+c-'0';
+c=gc();}
+return n;
+}
+ll n;
+void build(){
+    ll i;
+    f(i,n){
+
+    }
+}
+void update(){
+
+}
+
+void query(){
+
+}
+int main()
+{
+    n = uscan();
+    ll i,temp;
+    f(i, n)
+    {
+        temp=uscan();
+        
+    }
+
+    return 0;
+} 
---
+++ Templates/temp.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f(i,n) for(i=0;i<n;i++)
+#define pb push_back
+#define sd(n) scanf("%d",&n)
+#define sc(n) scanf("%c",&n)
+#define slld(n) scanf("%lld",&n)
+#define mod 1000000007
+#define mp make_pair
+#define ff first
+#define ss second
+#define ll long long
+#define ld long double
+#define gc getchar
+#define pc putchar
+inline ll uscan()
+{
+    ll n=0,c=gc();
+while(c<'0'||c>'9')
+c=gc();
+while(c<='9'&&c>='0'){
+n=n*10+c-'0';
+c=gc();}
+return n;
+}
+int main()
+{
+    int t=uscan();
+    while(t--){
+        ll n,i,j;
+    }
+	return 0;
+}
