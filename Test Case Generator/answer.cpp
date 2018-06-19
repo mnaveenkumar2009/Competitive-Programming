7f01c2ed74b0874acd6d58de3a4fe62854a1ab75
naveen <mnaveenkumar2009@gmail.com>
2018-06-19 13:14:36 +0530

    kh

--- Test Case Generator/answer.cpp
+++ Test Case Generator/answer.cpp
@@ -2,50 +2,54 @@
 using namespace std;
 #define f(i,n) for(i=0;i<n;i++)
 #define pb push_back
-#define mod 1000000007
-#define mp make_pair
-#define ff first
-#define ss second
-#define ll long long
-#define ld long double
-#define gc getchar
-#define pc putchar
-#define NIL -1
-inline ll uscan()
-{
-    ll n=0,c=gc();
-while(c<'0'||c>'9')
-c=gc();
-while(c <= '9'&&c>='0'){
-n=n*10+c-'0';
-c=gc();}
-return n;
-}
-
-ll pr(ll x, ll y)
-{
-    ll res = 1; 
-
-    x = x % mod;
-
-    while (y > 0)
+         
+int main() {
+    int q,i=0,t;
+    long long n,m;
+    scanf("%d",&t);
+    while(t--)
     {
-        if (y & 1)
-            res = (res*x) % mod;
-
-        y = y>>1; // y = y/2
-        x = (x*x) % mod;  
+        int n,k,c;
+        cin>>n>>k>>c;
+        vector <long long> a(n);
+        f(i,n)
+        {
+            scanf("%lld",&a[i]);
+        }
+        sort(a.begin(),a.end());
+        int count=0;
+        bool we=0;
+        while(1){
+            int y=k-1;
+            i=0;
+            while(y--){
+                int yy=a[i];
+                a.erase(a.begin()+i);
+                int low = 0, high = a.size();
+                while (low != high) {
+                    int mid = (low + high) / 2; 
+                    if (a[mid] < yy*c) {
+                        low = mid + 1;
+                    }
+                    else {
+                        high = mid;
+                    }
+                }
+                i=high;
+                if(i==a.size()){
+                    we=1;
+                    break;
+                }
+            }
+            if(!we)
+            {
+                a.erase(a.begin()+i);
+            }
+            if(!we)
+            count++;
+            if(a.size()==0||we)break;
+        } 
+        cout<<count<<endl;     
     }
-    return res;
-}
-
-int main()
-{
-    ll t=uscan(),kkk;
-    
-    f(kkk,t){
-    }
-	return 0;
-}
-
-
+    return 0;
+}   
