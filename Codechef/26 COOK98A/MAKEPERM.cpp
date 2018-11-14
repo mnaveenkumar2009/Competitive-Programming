fe4ebc279cc765f4a60aa1482b6d6a5892fa262c
Naveen <mnaveenkumar2009@gmail.com>
2018-11-14 12:54:37 +0530

    COOK98A added

---
+++ Codechef/26 COOK98A/MAKEPERM.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f(i,n) for(i=0;i<n;i++)
+#define pb push_back
+#define mod 1000000007
+#define mp make_pair
+#define ff first
+#define ss second
+#define ll long long
+#define ld long double
+#define gc getchar
+#define pc putchar
+#define sort1(a) sort(a.begin(),a.end())
+#define sort2(a,n) sort(a,a+n)
+
+inline ll uscan()
+{
+    ll n=0,c=gc();
+    bool check=0;
+    if(c=='-')check=1;
+    while(c<'0'||c>'9')
+    {
+        c=gc();
+        if(c=='-')check=1;
+    }
+    while(c<='9'&&c>='0'){
+        n=n*10+c-'0';
+        c=gc();
+    }
+    return n+(-2*check*n);
+}
+#define sc uscan()
+
+void prin(vector <ll> a){
+    ll i;
+    f(i,a.size()){
+        cout<<a[i]<<' ';
+    }pc('\n');
+}
+void prin(vector <vector <ll> > a){
+    ll i,j;
+    f(i,a.size()){
+        f(j,a[i].size())
+            cout<<a[i][j]<<' ';
+        pc('\n');
+    }
+    pc('\n');
+}
+void prin(vector < pair<ll,ll> > a){
+    ll i,j;
+    f(i,a.size()){
+        cout<<a[i].ff<<' '<<a[i].ss<<'\n';
+    }
+    pc('\n');
+}
+
+int main()
+{
+    ll t=sc;
+    while(t--){
+        ll n=sc,i,j,k;
+        vector <ll> a(n),b(n,1);
+        f(i,n){
+            a[i]=sc;
+            if(a[i]<=n)
+            b[a[i]-1]=0;
+        }
+        ll ans=0;
+        f(i,n)ans+=b[i];
+        cout<<ans<<'\n';
+    }
+    return 0;
+}
